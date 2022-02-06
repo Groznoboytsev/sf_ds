38f6bdc37f229e7581e9102be7ac2cb0439eb3d9
groznoboytsev-79SF <nikitospavlov@gmail.com>
2022-02-06 22:09:15 +0300

    random_predict function changed

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,48 +1,60 @@
-"""Игра угадай число.
-Компьютер сам загадывает и угадывает число
+"""Guess the number game.
+The computer itself guesses and guesses the number
 """
 
 from itertools import count
 import numpy as np
 
-def random_predict (number:int=1) -> int:
-    """Рандомно угадывает число
+
+
+def random_predict (number:int=np.random.randint(1,101)) ->int:
+    """Guesses the number in the minimum number of attempts
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): Intented number. Defaults to np.random.randint(1,101).
 
     Returns:
-        int: Число попыток
+        int: Number of attempts
     """
     
-    count = 0
+    up_limit = 101 # upper limit of expected number
+    down_limit = 0 # lower bound of expected number
+    count = 1
+    predict = 50
     
-    while True:
+    while number != predict:
         count += 1
-        predict_number = np.random.randint(1,101)
-        if number == predict_number:
-            break
+        
+        if number > predict:
+            down_limit = predict # overwrites the variable
+            predict = int((predict + up_limit)/2) # estimated number
+        
+        elif number < predict:
+            up_limit = predict
+            predict = int((predict + down_limit)/2)
+  
+            
     return count
 
 print(f'Количество попыток: {random_predict()}')
 
 def score_game (random_predict) -> int:
-    """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+    """Runs the game 1000 times, displays the average of the attempts
 
     Args:
-        random_predict ([type]): функция угадывает
+        random_predict ([type]): function guesses
 
     Returns:
-        int: среднее количество попыток
+        int: average number of attempts
     """
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид лоя воспроводимости
-    random_array = np.random.randint(1,101,size=1000) # загадали список чисесл
+    count_ls = [] # list to save the number of attempts
+    np.random.seed(1) # fix RANDOM SEED
+    random_array = np.random.randint(1,101,size=1000) # made a list of numbers
     
     for number in random_array:
         count_ls.append(random_predict(number))
         
-    score = int(np.mean(count_ls)) # находим сроеднее количество попыток
+    score = int(np.mean(count_ls)) # find the average number of attempts
     
     print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
